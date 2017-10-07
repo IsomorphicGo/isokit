126c51ad8e52a908bdf3fbf21e9fabcf8d713aad
Kamesh Balasubramanian <kamesh@wirecog.com>
2017-10-07 11:38:49 -0700

    Added notice

--- static.go
+++ static.go
@@ -1,3 +1,8 @@
+// The Isomorphic Go Project
+// Copyright (c) Wirecog, LLC. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license, which can be found in the LICENSE file.
+
 package isokit
 
 import (
