30d2263362a94d62b809801a9f95c76bf909e890
Kamesh Balasubramanian <kamesh@wirecog.com>
2017-12-30 14:06:07 -0800

    Making pathing agnostic

--- templatebundle.go
+++ templatebundle.go
@@ -58,11 +58,11 @@ func (t *TemplateBundle) importTemplateFileContents(templatesPath string) error 
 func (t *TemplateBundle) importTemplateFileContentsForCog(templatesPath string, prefixName string, templateFileExtension string) error {
 
 	templateDirectory := filepath.Clean(templatesPath)
-	RegisterStaticAssetsSearchPath(strings.Replace(templateDirectory, string(os.PathSeparator)+"templates", "", -1))
+	RegisterStaticAssetsSearchPath(strings.Replace(templateDirectory, "/"+"templates", "", -1))
 	//println("td: ", templateDirectory)
 	if err := filepath.Walk(templateDirectory, func(path string, info os.FileInfo, err error) error {
 		if strings.HasSuffix(path, templateFileExtension) {
-			name := strings.TrimSuffix(strings.TrimPrefix(path, templateDirectory+string(os.PathSeparator)), TemplateFileExtension)
+			name := strings.TrimSuffix(strings.TrimPrefix(path, templateDirectory+"/"), TemplateFileExtension)
 			name = prefixName + "/" + name
 			contents, err := ioutil.ReadFile(path)
 			t.items[name] = string(contents)
